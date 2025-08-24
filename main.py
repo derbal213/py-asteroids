001d707b0ff14ee4e232de923816c83bb285b64d
Derek <git@derekball.net>
2025-08-24 12:16:52 -0400

    Switch draw and updates to use groups

--- main.py
+++ main.py
@@ -10,15 +10,29 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while (True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
 
-        player.update(dt)
-        player.draw(screen)
+        #player.update(dt)
+        #player.draw(screen)
+
+        #Update
+        updatable.update(dt)
+
+        #Draw
+        for draw in drawable:
+            draw.draw(screen)
 
         pygame.display.flip()
         timePassed = clock.tick(60)
--- player.py
+++ player.py
@@ -3,6 +3,8 @@ from circleshape import CircleShape
 from constants import *
 
 class Player(CircleShape):
+    containers = None
+
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
